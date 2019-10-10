b64145d5cfa3c0dd82f6a7b76d5c5139d5e54291
Albiorix Technology <hiren.ghodasara@outlook.com>
2019-10-10 17:41:20 +0530

    Updates

---
+++ precache-manifest.958520f9db38fb6e6ca8822d9bf172b9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21a8cc13c2023e91c3d8fa1c2d61924",
+    "url": "/qr-login-web/index.html"
+  },
+  {
+    "revision": "18594b5b5d4a61b98487",
+    "url": "/qr-login-web/static/css/0.aa994fe8.chunk.css"
+  },
+  {
+    "revision": "0b9759ef801768f6f835",
+    "url": "/qr-login-web/static/css/3.e4ab9787.chunk.css"
+  },
+  {
+    "revision": "870155765be884248ea9",
+    "url": "/qr-login-web/static/css/5.710cf7d9.chunk.css"
+  },
+  {
+    "revision": "a7e269c5a01bd66925c0",
+    "url": "/qr-login-web/static/css/main.107a33f2.chunk.css"
+  },
+  {
+    "revision": "18594b5b5d4a61b98487",
+    "url": "/qr-login-web/static/js/0.d3fc8137.chunk.js"
+  },
+  {
+    "revision": "0b9759ef801768f6f835",
+    "url": "/qr-login-web/static/js/3.93795d90.chunk.js"
+  },
+  {
+    "revision": "9f239f3cab96a1325526",
+    "url": "/qr-login-web/static/js/4.cbf6b95f.chunk.js"
+  },
+  {
+    "revision": "870155765be884248ea9",
+    "url": "/qr-login-web/static/js/5.8bd1ab8d.chunk.js"
+  },
+  {
+    "revision": "dc4442928c286bed9fc3",
+    "url": "/qr-login-web/static/js/6.85b60603.chunk.js"
+  },
+  {
+    "revision": "27adc65b289bc3c70343",
+    "url": "/qr-login-web/static/js/7.b1272e19.chunk.js"
+  },
+  {
+    "revision": "a7e269c5a01bd66925c0",
+    "url": "/qr-login-web/static/js/main.1b0c9429.chunk.js"
+  },
+  {
+    "revision": "d103952d0c72ef39ccc5",
+    "url": "/qr-login-web/static/js/runtime~main.bd5618ba.js"
+  }
+]);
