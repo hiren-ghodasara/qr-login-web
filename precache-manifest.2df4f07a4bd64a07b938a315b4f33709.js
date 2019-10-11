466f6f43dfce63e7419f7ffa4d770de8958187ac
Albiorix Technology <hiren.ghodasara@outlook.com>
2019-10-11 15:20:07 +0530

    Updates

--- precache-manifest.2df4f07a4bd64a07b938a315b4f33709.js
+++ precache-manifest.2df4f07a4bd64a07b938a315b4f33709.js
@@ -1,14 +1,14 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c39562344897142519dc2e97a855dbf9",
+    "revision": "8218061c1151dc2bf60ab6ff50ff36fe",
     "url": "/qr-login-web/index.html"
   },
   {
-    "revision": "7524eb695d74d180d677",
+    "revision": "18594b5b5d4a61b98487",
     "url": "/qr-login-web/static/css/0.aa994fe8.chunk.css"
   },
   {
-    "revision": "4c72db774c47b3840f51",
+    "revision": "0b9759ef801768f6f835",
     "url": "/qr-login-web/static/css/3.e4ab9787.chunk.css"
   },
   {
@@ -16,16 +16,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/qr-login-web/static/css/5.710cf7d9.chunk.css"
   },
   {
-    "revision": "dcd8eb805d9311dc02f0",
+    "revision": "be7f02f1f403cb21dc96",
     "url": "/qr-login-web/static/css/main.107a33f2.chunk.css"
   },
   {
-    "revision": "7524eb695d74d180d677",
-    "url": "/qr-login-web/static/js/0.985dbd99.chunk.js"
+    "revision": "18594b5b5d4a61b98487",
+    "url": "/qr-login-web/static/js/0.d3fc8137.chunk.js"
   },
   {
-    "revision": "4c72db774c47b3840f51",
-    "url": "/qr-login-web/static/js/3.5500e2f0.chunk.js"
+    "revision": "0b9759ef801768f6f835",
+    "url": "/qr-login-web/static/js/3.93795d90.chunk.js"
   },
   {
     "revision": "9f239f3cab96a1325526",
@@ -44,11 +44,11 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/qr-login-web/static/js/7.0358168e.chunk.js"
   },
   {
-    "revision": "dcd8eb805d9311dc02f0",
-    "url": "/qr-login-web/static/js/main.be3b5b31.chunk.js"
+    "revision": "be7f02f1f403cb21dc96",
+    "url": "/qr-login-web/static/js/main.7d31b457.chunk.js"
   },
   {
-    "revision": "737bbbe3b188b4e45d22",
-    "url": "/qr-login-web/static/js/runtime~main.48b51dcf.js"
+    "revision": "b86cb13713e29c2ccc5f",
+    "url": "/qr-login-web/static/js/runtime~main.93ab38ba.js"
   }
 ]);
