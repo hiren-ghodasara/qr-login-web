365683812cf7e79f7d32521fad8ab27959b6bfec
Hiren Ghodasara <hiren.ghodasara@outlook.com>
2019-11-06 12:00:48 +0530

    Updates

---
+++ precache-manifest.2f36ee1d83739745dc96f4e6c52481c7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fbf60b3811a24877c712d979e73d079",
+    "url": "/qr-login-web/index.html"
+  },
+  {
+    "revision": "194064dfb507dce9f9a5",
+    "url": "/qr-login-web/static/css/2.6b2167b0.chunk.css"
+  },
+  {
+    "revision": "ec04138997e0ba9b7937",
+    "url": "/qr-login-web/static/css/3.4909d231.chunk.css"
+  },
+  {
+    "revision": "f10394b054c8299015ac",
+    "url": "/qr-login-web/static/css/main.32a6646f.chunk.css"
+  },
+  {
+    "revision": "194064dfb507dce9f9a5",
+    "url": "/qr-login-web/static/js/2.cd2bf04a.chunk.js"
+  },
+  {
+    "revision": "ec04138997e0ba9b7937",
+    "url": "/qr-login-web/static/js/3.b3177ea6.chunk.js"
+  },
+  {
+    "revision": "55954a110783df67cbca",
+    "url": "/qr-login-web/static/js/4.b93871c2.chunk.js"
+  },
+  {
+    "revision": "f10394b054c8299015ac",
+    "url": "/qr-login-web/static/js/main.ed293679.chunk.js"
+  },
+  {
+    "revision": "c372d79bf8bf35237290",
+    "url": "/qr-login-web/static/js/runtime~main.17e2f5fb.js"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/qr-login-web/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/qr-login-web/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "9d204f50ec8e5bcc5dcda0132d404b48",
+    "url": "/qr-login-web/static/media/fa-brands-400.9d204f50.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/qr-login-web/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/qr-login-web/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/qr-login-web/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/qr-login-web/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "3d102342391af184d5ae9e7708d8220f",
+    "url": "/qr-login-web/static/media/fa-solid-900.3d102342.svg"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/qr-login-web/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/qr-login-web/static/media/fa-solid-900.8e4a6dcc.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/qr-login-web/precache-manifest.6fc2694d7815c46b948254599ccd1911.js"
+  "/qr-login-web/precache-manifest.2f36ee1d83739745dc96f4e6c52481c7.js"
 );
 
 self.addEventListener('message', (event) => {
